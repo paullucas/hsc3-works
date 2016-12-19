5a4868d9bfc1713b17e1f49aa489ddfb33a8c97b
Paul <paulll.lucas@gmail.com>
2016-12-19 09:58:09 -0800

    Init commit

---
+++ 1.hs
@@ -0,0 +1,13 @@
+-- :load Boot
+-- boot
+-- C-c >
+
+import Sound.SC3
+import Sound.SC3.ID
+
+withSC3 (send (g_new [(1, AddToTail, 0)]))
+audition (out 0 (sinOsc AR 440 0 * 0.1))
+withSC3 (send (n_free [1]))
+
+let g = withSC3 (send (g_new [(1, AddToTail, 0)]))
+let s freq = audition (out 0 (sinOsc AR freq 0 * 0.1))
