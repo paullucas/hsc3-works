80366f5974b8d2e1d9061661de79f482e60343e6
Paul <paulll.lucas@gmail.com>
2016-12-20 12:23:23 -0800

    Update 1.hs

--- 1.hs
+++ 1.hs
@@ -1,6 +1,8 @@
 import Sound.SC3
 :load functions.hs
 
+let f = map ("/home/paulll/Producing/duplicates/48/wav/" ++) ["1-4.wav", "1-7.wav"]
+
 g
 o (sw 100 0.1)
 o (s 220 0.1)
@@ -9,7 +11,8 @@ mono (s 220 0.1)
 k
 
 g
-rb 0 "/home/paulll/Producing/duplicates/48/wav/1-4.wav"
+rb 0 (f !! 0)
+rb 1 (f !! 1)
 o (lp 0 1 0.5)
-o (lpm 0 1 0.5)
+o (lp 1 1 0.5)
 k
