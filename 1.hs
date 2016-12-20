cd5c6afbbd677ac2d75089fcec0823bb22af1aca
Paul <paulll.lucas@gmail.com>
2016-12-20 02:03:47 -0800

    Add o, rb, lp, lpm funcs

--- 1.hs
+++ 1.hs
@@ -1,11 +1,15 @@
 import Sound.SC3
 :load functions.hs
 
-withSC3 (send (g_new [(1, AddToTail, 0)]))
-audition (out 0 (sinOsc AR 440 0 * 0.1))
-withSC3 (send (n_free [1]))
-
 g
+o (sw 100 0.1)
+o (s 220 0.1)
 mono (sw 100 0.1)
 mono (s 220 0.1)
+k
+
+g
+rb 0 "/home/paulll/Producing/duplicates/48/wav/1-4.wav"
+o (lp 0 1 0.5)
+o (lpm 0 1 0.5)
 k
--- functions.hs
+++ functions.hs
@@ -2,6 +2,33 @@ import Sound.SC3
 
 g = withSC3 (send (g_new [(1, AddToTail, 0)]))
 k = withSC3 (send (n_free [1]))
+
+o x = audition x
 mono x = audition $ mce [out 0 x, out 1 x]
+
 s f a = (sinOsc AR f 0 * a)
 sw f a = (saw AR f) * a
+
+rb bn fn =
+  withSC3 (do
+    async (b_allocRead bn fn 0 0))
+
+lp b r ph  =
+  bufRdC 2 AR b ((lfSaw AR (r * 0.5/bufDur KR b) 0 + ph) * bufFrames KR b) Loop
+
+lpm b r ph =
+  bufRdC 1 AR b ((lfSaw AR (r * 0.5/(bufDur KR b)) 0 + ph) * bufFrames KR b) Loop
+
+-- lp b r ph  =
+--   let dur = bufDur KR b
+--       fr = bufFrames KR b
+--       pos = (lfSaw AR (r * 0.5/dur) 0 + ph) * fr
+--   in
+--   bufRdC 2 AR b pos Loop
+
+-- lpm b r ph  =
+--   let dur = bufDur KR b
+--       fr = bufFrames KR b
+--       pos = (lfSaw AR (r * 0.5/dur) 0 + ph) * fr
+--   in
+--   bufRdC 1 AR b pos Loop
