5011d96bf1dc2a9a4a07c6d850f73304c0690563
Paul <paulll.lucas@gmail.com>
2017-03-03 03:11:39 -0800

    OSX friendly

--- Main.hs
+++ Main.hs
@@ -7,6 +7,6 @@ main :: IO ()
 main = do
     system "scsynth -u 57110 &"
     system "sleep 5"
-    system "jack_connect SuperCollider:out_1 system:playback_1"
-    system "jack_connect SuperCollider:out_2 system:playback_2"
+--    system "jack_connect SuperCollider:out_1 system:playback_1"
+--    system "jack_connect SuperCollider:out_2 system:playback_2"
     putStrLn "Ok!"
