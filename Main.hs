076962e1eec6a38568ad608f657b21a8c08e8ddf
Paul <paulll.lucas@gmail.com>
2017-04-22 18:00:12 -0700

    Fix Main.hs

--- Main.hs
+++ Main.hs
@@ -7,6 +7,6 @@ main :: IO ()
 main = do
     system "scsynth -u 57110 &"
     system "sleep 5"
-    system "[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_1 system:playback_1"
-    system "[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_2 system:playback_2"
+    system "/bin/bash -c \"[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_1 system:playback_1\""
+    system "/bin/bash -c \"[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_2 system:playback_2\""
     putStrLn "Ok!"
