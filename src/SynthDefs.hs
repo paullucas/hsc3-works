153b7488ba0987c35f3ddf4b2641b164d8789489
Paul <paulll.lucas@gmail.com>
2017-05-08 11:32:15 -0700

    Refactor SynthDefs & UGens

--- src/SynthDefs.hs
+++ src/SynthDefs.hs
@@ -9,56 +9,31 @@ import Sound.SC3.UGen.Bindings.DB hiding (lpf, hpf, limiter, freeVerb, gVerb)
 -- SynthDefs for c6.hs
 --
 
-c4sio :: Double -> Double -> Double -> IO Int
-c4sio f a rl =
-  sd "c4sio"
-  $ amp ampLevel
-  $ env gate 15 release
+c6s :: Num b => Double -> Double -> Double -> IO b
+c6s frequency db release =
+  sd "c6s"
+  $ o (k "a" db)
+  $ env (k "g" 1) 15 (k "rl" release)
   $ limiter 0.8 0.001
   $ sinOsc AR (mce [freq, freq + 1]) 1
   where
-    freq     = k "f" f
-    ampLevel = k "a" a
-    release  = k "rl" rl
-    gate     = k "g" 1
-
-c5wr :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> IO Int
-c5wr b p a fs ws lff hff =
-  sd "c5wr"
-  $ amp ampLevel
-  $ env gate 15 40
-  $ freeVerb 1 1 1
-  $ hpf highpassFreq
-  $ lpf lowpassFreq
-  $ warp1 2 buffer pointer freqScale windowSize (-1) overlaps 0.0 4
-  where
-    buffer       = k "b" b
-    pointer      = k "p" p
-    ampLevel     = k "a" a
-    freqScale    = k "fs" fs
-    windowSize   = k "ws" ws
-    lowpassFreq  = k "lff" lff
-    highpassFreq = k "hff" hff
-    overlaps     = k "ol" 1
-    gate         = k "g" 1
+    freq = k "f" frequency
 
 c6w :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double -> Double -> IO Int
-c6w b p a fs ws lff hff at rl =
+c6w buffer pointer db freqScale windowSize lpF hpF attack release =
   sd "c6w"
-  $ amp ampLevel
-  $ env gate attack release
-  $ hpf highpassFreq
-  $ lpf lowpassFreq
-  $ warp1 2 buffer pointer freqScale windowSize (-1) overlaps 0.0 4
-  where
-    buffer       = k "b" b
-    pointer      = k "p" p
-    ampLevel     = k "a" a
-    freqScale    = k "fs" fs
-    windowSize   = k "ws" ws
-    lowpassFreq  = k "lff" lff
-    highpassFreq = k "hff" hff
-    attack       = k "at" at
-    release      = k "rl" rl
-    overlaps     = k "ol" 1
-    gate         = k "g" 1
+  $ o (k "a" db)
+  $ env (k "g" 1) (k "at" attack) (k "rl" release)
+  $ hpf (k "hff" hpF)
+  $ lpf (k "lff" lpF)
+  $ warp1 2 (k "b" buffer) (k "p" pointer) (k "fs" freqScale) (k "ws" windowSize) (-1) (k "ol" 1) 0.0 4
+
+c6wr :: Double -> Double -> Double -> Double -> Double -> Double -> Double -> IO Int
+c6wr buffer pointer db freqScale windowSize lpF hpF =
+  sd "c6wr"
+  $ o (k "a" db)
+  $ env (k "g" 1) 15 40
+  $ freeVerb 1 1 1
+  $ hpf (k "hff" hpF)
+  $ lpf (k "lff" lpF)
+  $ warp1 2 (k "b" buffer) (k "p" pointer) (k "fs" freqScale) (k "ws" windowSize) (-1) (k "ol" 1) 0.0 4
--- src/UGens.hs
+++ src/UGens.hs
@@ -10,31 +10,25 @@ env :: UGen -> UGen -> UGen -> UGen -> UGen
 env gate attack release input =
   input * envGen KR gate 1 0 1 RemoveSynth (envASR attack 1 release EnvLin)
 
-tgrain :: UGen -> UGen -> UGen -> UGen -> UGen
-tgrain bufNum rate centerPos duration =
-  tGrains 2 (impulse AR 4 0) bufNum rate centerPos duration 0 1 1
-
-tgrain' :: UGen -> UGen -> UGen -> UGen -> UGen -> UGen
-tgrain' bufNum rate centerPos duration ampLevel =
-  tGrains 2 (impulse AR 4 0) bufNum rate centerPos duration 0 ampLevel 1
-
-pan :: UGen -> UGen
-pan input = pan2 input 0 1
-
-amp :: UGen -> UGen -> UGen
-amp ampLevel input = out 0 $ input * ampLevel
+o :: UGen -> UGen -> UGen
+o db input = out 0 $ input * db
 
 lpf :: UGen -> UGen -> UGen
-lpf freq in_ = mkUGen Nothing [KR,AR] (Right [0]) "LPF" [in_,freq] Nothing 1 (Special 0) NoId
+lpf freq in_ =
+  mkUGen Nothing [KR,AR] (Right [0]) "LPF" [in_,freq] Nothing 1 (Special 0) NoId
 
 hpf :: UGen -> UGen -> UGen
-hpf freq in_ = mkUGen Nothing [KR,AR] (Right [0]) "HPF" [in_,freq] Nothing 1 (Special 0) NoId
+hpf freq in_ =
+  mkUGen Nothing [KR,AR] (Right [0]) "HPF" [in_,freq] Nothing 1 (Special 0) NoId
 
 limiter :: UGen -> UGen -> UGen -> UGen
-limiter level dur in_ = mkUGen Nothing [AR] (Right [0]) "Limiter" [in_,level,dur] Nothing 1 (Special 0) NoId
+limiter level dur in_ =
+  mkUGen Nothing [AR] (Right [0]) "Limiter" [in_,level,dur] Nothing 1 (Special 0) NoId
 
 freeVerb :: UGen -> UGen -> UGen -> UGen -> UGen
-freeVerb mix room damp in_ = mkUGen Nothing [AR] (Right [0]) "FreeVerb" [in_,mix,room,damp] Nothing 1 (Special 0) NoId
+freeVerb mix room damp in_ =
+  mkUGen Nothing [AR] (Right [0]) "FreeVerb" [in_,mix,room,damp] Nothing 1 (Special 0) NoId
 
 gVerb :: UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen
-gVerb roomsize revtime damping inputbw spread drylevel earlyreflevel taillevel maxroomsize in_ = mkUGen Nothing [AR] (Right [0]) "GVerb" [in_,roomsize,revtime,damping,inputbw,spread,drylevel,earlyreflevel,taillevel,maxroomsize] Nothing 2 (Special 0) NoId
+gVerb roomsize revtime damping inputbw spread drylevel earlyreflevel taillevel maxroomsize in_ =
+  mkUGen Nothing [AR] (Right [0]) "GVerb" [in_,roomsize,revtime,damping,inputbw,spread,drylevel,earlyreflevel,taillevel,maxroomsize] Nothing 2 (Special 0) NoId
