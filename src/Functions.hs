6687b97f92e29c27709065fefc0e6240c54c6e74
Paul <paulll.lucas@gmail.com>
2017-05-11 03:47:58 -0700

    Remove absolute path from boot

--- src/Functions.hs
+++ src/Functions.hs
@@ -152,6 +152,6 @@ boot :: IO ()
 boot = do
     system "scsynth -u 57110 &"
     system "sleep 5"
-    system "/bin/bash -c \"[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_1 system:playback_1\""
-    system "/bin/bash -c \"[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_2 system:playback_2\""
+    system "bash -c \"[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_1 system:playback_1\""
+    system "bash -c \"[ \"$(uname -s)\" == \"Linux\" ] && jack_connect SuperCollider:out_2 system:playback_2\""
     putStrLn "Ok!"
