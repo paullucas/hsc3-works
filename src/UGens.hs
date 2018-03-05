c7c922c909873811ac64cc0cb2caad0af7e620a8
Paul <paulll.lucas@gmail.com>
2018-03-05 00:26:08 -0800

    Reformat UGens.hs

--- src/UGens.hs
+++ src/UGens.hs
@@ -16,23 +16,90 @@ o db input = out 0 $ input * db
 
 lpf :: UGen -> UGen -> UGen
 lpf freq in_ =
-  mkUGen Nothing [KR,AR] (Right [0]) "LPF" [in_,freq] Nothing 1 (Special 0) NoId
+  mkUGen
+    Nothing
+    [KR, AR]
+    (Right [0])
+    "LPF"
+    [in_, freq]
+    Nothing
+    1
+    (Special 0)
+    NoId
 
 hpf :: UGen -> UGen -> UGen
 hpf freq in_ =
-  mkUGen Nothing [KR,AR] (Right [0]) "HPF" [in_,freq] Nothing 1 (Special 0) NoId
+  mkUGen
+    Nothing
+    [KR, AR]
+    (Right [0])
+    "HPF"
+    [in_, freq]
+    Nothing
+    1
+    (Special 0)
+    NoId
 
 limiter :: UGen -> UGen -> UGen -> UGen
 limiter level dur in_ =
-  mkUGen Nothing [AR] (Right [0]) "Limiter" [in_,level,dur] Nothing 1 (Special 0) NoId
+  mkUGen
+    Nothing
+    [AR]
+    (Right [0])
+    "Limiter"
+    [in_, level, dur]
+    Nothing
+    1
+    (Special 0)
+    NoId
 
 freeVerb :: UGen -> UGen -> UGen -> UGen -> UGen
 freeVerb mix room damp in_ =
-  mkUGen Nothing [AR] (Right [0]) "FreeVerb" [in_,mix,room,damp] Nothing 1 (Special 0) NoId
+  mkUGen
+    Nothing
+    [AR]
+    (Right [0])
+    "FreeVerb"
+    [in_, mix, room, damp]
+    Nothing
+    1
+    (Special 0)
+    NoId
 
-gVerb :: UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen
+gVerb ::
+     UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
+  -> UGen
 gVerb roomsize revtime damping inputbw spread drylevel earlyreflevel taillevel maxroomsize in_ =
-  mkUGen Nothing [AR] (Right [0]) "GVerb" [in_,roomsize,revtime,damping,inputbw,spread,drylevel,earlyreflevel,taillevel,maxroomsize] Nothing 2 (Special 0) NoId
+  mkUGen
+    Nothing
+    [AR]
+    (Right [0])
+    "GVerb"
+    [ in_
+    , roomsize
+    , revtime
+    , damping
+    , inputbw
+    , spread
+    , drylevel
+    , earlyreflevel
+    , taillevel
+    , maxroomsize
+    ]
+    Nothing
+    2
+    (Special 0)
+    NoId
 
 pbuf :: UGen -> UGen -> UGen -> UGen -> UGen
-pbuf buffer rate trigger startPos = playBuf 2 AR buffer rate trigger startPos Loop RemoveSynth
+pbuf buffer rate trigger startPos =
+  playBuf 2 AR buffer rate trigger startPos Loop RemoveSynth
